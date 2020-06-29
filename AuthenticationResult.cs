c5fc1ebd45505b87dfceef731f16ec039bdb6130
MrJackSpade <loffredo.ap@gmail.com>
2020-06-29 09:09:09 -0700

    Add files via upload

---
+++ AuthenticationResult.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Penguin.Authentication.Abstractions
+{
+    public class AuthenticationResult
+    {
+        public bool IsValid { get; set; }
+        public Exception Exception { get; set; }
+    }
+}
---
+++ Interfaces/IAuthenticator.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Penguin.Authentication.Abstractions.Interfaces
+{
+    public interface IAuthenticator
+    {
+        Task<AuthenticationResult> Authenticate(string Username, string Password);
+    }
+}
