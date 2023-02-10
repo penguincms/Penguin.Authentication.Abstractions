4356c1c102d857bfcade9600006b23ff5ba5d3f6
Anthony Loffredo <loffredo.ap@gmail.com>
2023-02-10 13:28:02 -0700

    Autocommit
    Autocommit

--- AuthenticationResult.cs
+++ AuthenticationResult.cs
@@ -5,6 +5,7 @@ namespace Penguin.Authentication.Abstractions
     public class AuthenticationResult
     {
         public bool IsValid { get; set; }
+
         public Exception Exception { get; set; }
     }
 }
